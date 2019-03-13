e2c5f404e934cdb54fc3ee55d93205bfd21bfa3d
Yannaing Lynn <rghtbossy@gmail.com>
2019-03-13 11:07:21 +0630

    first

---
+++ app/src/main/java/com/softwarefactory/navigationcomponenttest/InsertFragment.kt
@@ -0,0 +1,30 @@
+package com.softwarefactory.navigationcomponenttest
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_insert.*
+
+class InsertFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_insert, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bt_second.setOnClickListener {
+            val nextAction = InsertFragmentDirections.nextAction()
+            Navigation.findNavController(it).navigate(nextAction)
+        }
+    }
+
+}
